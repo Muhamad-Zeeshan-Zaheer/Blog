4f2aa37e862060f10dc9a58a8f5338e9a0630765
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2026-01-14 13:04:22 +0500

    Added the index and show action with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
+  resources :articles
 
   get "up" => "rails/health#show", as: :rails_health_check
 
